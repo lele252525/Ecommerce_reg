7756608878aff1bac0089c766ae47de665369b42
lele252525 <costadaniele2016@gmail.com>
2025-09-10 11:19:09 +0200

    Configurazione

---
+++ src/test/java/com/costa/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.costa.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
